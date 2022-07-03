1c70ed0bf987f5b6e631ebceacad325bad22f7ab
unknown <joaoantoniofer@gmail.com>
2022-07-02 23:12:33 -0300

    GridLoyout1 02/07/22

--- scripts/main.py
+++ scripts/main.py
@@ -3,13 +3,41 @@ from kivy.core.window import Window
 
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
+
+class GridLayouttExample(GridLayout):
+    def print_this(self):
+        print("this button is "+str(self.text))
+        pass
+
+    def add_buttons(self, number):
+        i = 0
+        while(i<number):
+            b = Button(text=str(i))
+            b.bind(on_press=GridLayouttExample.print_this)
+            self.add_widget(b)
+            print("another button "+str(i))
+            i+=1
+            pass
+        pass    
+          
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        GridLayouttExample.add_buttons(self, 100)
+        pass
+
+      
+
 
 
 class BoxLayoutExample(BoxLayout):
     pass
-
-    """ def __init__(self, **kwargs):
+    """
+    #structure to display boxlayout with buttons
+    def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.orientation = "vertical"
 
@@ -21,6 +49,7 @@ class BoxLayoutExample(BoxLayout):
         self.add_widget(b2)
         self.add_widget(b3)
 
+    #funtion that create and display varius buttons
     def add_buttons(self, number):
         i = 0
         while(i<number):
